fc7f57f5e44d69ef8cf9a54888859a17672cce32
henriquevoigt <henriqueftk@gmail.com.com>
2026-01-12 20:16:28 -0300

    more leetcode

---
+++ AED1/leetcode/206.c
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* reverseList(struct ListNode* head) {
+    
+    struct ListNode* anterior = NULL;
+    struct ListNode* atual = head;
+    struct ListNode* proximo = NULL; 
+
+    while ( atual != NULL ) {
+        proximo = atual->next;
+        atual->next = anterior;
+        anterior = atual;
+        atual = proximo;
+    }
+
+    return anterior;
+}
